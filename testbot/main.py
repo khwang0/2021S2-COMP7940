fea708255ca8c6e2aa500fdc088739a1082c5565
ZijianLei <cszjlei@comp.hkbu.edu.hk>
2021-01-18 16:36:10 +0800

    lab 3 init

---
+++ testbot/main.py
@@ -0,0 +1,24 @@
+import telegram
+from telegram.ext import Updater,CommandHandler,MessageHandler, Filters
+import configparser
+import logging
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    bot = telegram.Bot(token=(config['TELEGRAM']['ACCESS_TOKEN']))
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    #the logging helps you know when things do not work as expected
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    dispatcher = updater.dispatcher
+    updater.start_polling() # To start the bot, run:
+    # print(bot.logg)
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+if __name__ == '__main__':
+    main()
