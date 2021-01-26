e6020f8204ba6a42d84a907eb4dfd2ed5df03c63
ZijianLei <cszjlei@comp.hkbu.edu.hk>
2021-01-26 11:43:33 +0800

    lab3 update

--- testbot/main.py
+++ testbot/main.py
@@ -1,20 +1,28 @@
 import telegram
-from telegram.ext import Updater,CommandHandler,MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters
+# The MassageHandler is used for all massage updates
 import configparser
 import logging
+
+
 def main():
+    # Load your token and creat an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    #the logging helps you know when things do not work as expected
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
-    updater.start_polling() # To start the bot, run:
-
+    # You can set this logging module, so you will know when and why things do not work as expected
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    # register a dispatcher to handle massage:
+    # register an echo dispatcher
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     dispatcher.add_handler(echo_handler)
+
+    # To start the bot:
+    updater.start_polling()
     updater.idle()
+
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
