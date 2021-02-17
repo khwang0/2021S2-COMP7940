8fac79e484310d27126a00148e96961e62448f9e
Kevin Wang <khwang0@gmail.com>
2021-02-18 00:26:37 +0800

    update chatbot

--- lab5/chatbot.py
+++ lab5/chatbot.py
@@ -6,7 +6,6 @@ import logging
 import redis
 
 global redis1
-
 def main():
     # Load your token and create an Updater for your Bot
     
@@ -16,7 +15,8 @@ def main():
     dispatcher = updater.dispatcher
 
     global redis1
-    redis1 = redis.Redis(host=(os.environ['HOST']), password=(os.environ['PWD']), port=(os.environ['REDISPORT']))
+
+    redis1 = redis.Redis(host=(os.environ['HOST']), password=(os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
 
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
